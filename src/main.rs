57d996d4b1a43850ac436b329e5bb1e6fdea200f
Konstantin Welke <konstantin.welke@citrix.com>
2016-04-05 10:58:49 +0200

    add back #[cfg(target_os="macos")], might even compile on linux ;)

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,12 @@ use std::env::args;
 use std::ffi::CString;
 use libc::{c_int, size_t};
 
+#[cfg(target_os="macos")]
+use std::boxed::Box;
+
+#[cfg(target_os="macos")]
+use std::ops::DerefMut;
+
 extern crate libc;
 
 fn main() {
@@ -32,9 +38,33 @@ fn open(path: &str, flags: c_int) -> c_int {
 	ret
 }
 
-fn sendfile(src: c_int, dst: c_int, count: size_t) {
+#[cfg(target_os="macos")]
+struct Sendfile (Box<libc::off_t>);
+
+#[cfg(target_os="macos")]
+fn sendfile(src: c_int, dst: c_int, _: size_t) -> Sendfile {
+	println!("copying fd {} to fd {}", src, dst);
+	let mut sf = Sendfile( Box::new(0) );
+	let result = unsafe {
+		libc::sendfile(src, 
+			dst, 
+			0, 
+			sf.0.deref_mut() as *mut libc::off_t, 
+			std::ptr::null_mut(), 
+			0)
+	};
+	println!("sendfile() returned {}", result);
+	sf
+}
+
+#[cfg(target_os="linux")]
+struct Sendfile;
+
+#[cfg(target_os="linux")]
+fn sendfile(src: c_int, dst: c_int, count: size_t) -> Sendfile{
 	let result = unsafe {
 		libc::sendfile(dst, src, std::ptr::null_mut(), count)
 	};
 	println!("sendfile() returned {}", result);
+	Sendfile
 }
